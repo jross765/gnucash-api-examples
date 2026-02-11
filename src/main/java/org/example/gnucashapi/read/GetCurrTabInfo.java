7ff2012c0700a3596ce7789ea1038370929c1a5a
James Rossini <james.rossini@posteo.de>
2026-02-11 10:28:30 +0100

    GetCurrTabInfo: New

---
+++ src/main/java/org/example/gnucashapi/read/GetCurrTabInfo.java
@@ -0,0 +1,32 @@
+package org.example.gnucashapi.read;
+
+import java.io.File;
+
+import org.gnucash.api.pricedb.ComplexPriceTable;
+import org.gnucash.api.read.impl.GnuCashFileImpl;
+
+public class GetCurrTabInfo {
+    // BEGIN Example data -- adapt to your needs
+    private static String gcshFileName = "example_in.xml";
+    // END Example data
+
+    // -----------------------------------------------------------------
+
+    public static void main(String[] args) {
+    	try {
+    		GetCurrTabInfo tool = new GetCurrTabInfo();
+    		tool.kernel();
+    	} catch (Exception exc) {
+    		System.err.println("Execution exception. Aborting.");
+    		exc.printStackTrace();
+    		System.exit(1);
+    	}
+    }
+
+    protected void kernel() throws Exception {
+    	GnuCashFileImpl gcshFile = new GnuCashFileImpl(new File(gcshFileName));
+
+    	ComplexPriceTable tab = gcshFile.getCurrencyTable();
+    	System.out.println(tab.toString());
+    }
+}
